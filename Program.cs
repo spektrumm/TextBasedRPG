b965be9f1eb74085b539986d02ba2d215eeeb449
nate hare (spekky) <51189697+spektrumm@users.noreply.github.com>
2021-07-06 00:41:45 -0700

    Character selection and Title updating implementation

--- Program.cs
+++ Program.cs
@@ -3,33 +3,20 @@
 namespace TextBasedRPG
 {
     class charClass {
-
-        public string name;
         public string classIn;
         public string userClass;
-        private float experience;
-        private float resource;
-        public string rngWeaponName;
-        public string[] eqWeapon = {" "};
-
-        public string[] weapName = {""};
-        
-        //public string rngWeapon(string rngWeaponName) {}
-        
-
-        public void Mage(string charName) {
+        //public string[] weapName = {""};
+        public int numGenerator() {
 
             // random number generator
             int numSelect = 0;
             Random ranNum = new Random();
             numSelect = ranNum.Next(1,11);
 
-            experience = 0f; // set char xp to 0
-            rngWeaponName = weapName[numSelect]; // taking random number to index a list of pre generated weapon names for starter weapon
+            return numSelect;
 
-            name = charName;
+            
         }
-
         public void selectClass() {
 
             // get character name
@@ -44,21 +31,30 @@ namespace TextBasedRPG
 
             switch (userClass) {
                 case "mage":
-                    // call new random mage weapon assign
-                    //call new Mage(); function w given name from charName
-                    Console.WriteLine($"{charName}, your new Mage, has just been created.");
+                    
+                    int randomNum = numGenerator(); // call rng num method
+                    Mage mage01 = new Mage(charName, randomNum); // call new mage constructor
+                    
+                    // print a line to the user describing the new Mage they have just created.
+                    Console.WriteLine($"{mage01.mageName}, your new Mage, has just been created.\nThey have {mage01.Mana} mana points, and are using {mage01.rngWeaponNameMage} as their weapon of choice.");
+                    
+                    //write to file to save this data for use in the rest of the game.
+
                     break;
                 case "rogue":
                     // call new random rogue weapon assign
                     //call new Rogue(); function w given name from charName
+                    Console.WriteLine($"{charName}, your new Rogue, has just been created.");
                     break;
                 case "duelist":
                     // call new random duelist weapon assign
                     //call new Duelist(); function w given name from charName
+                    Console.WriteLine($"{charName}, your new Duelist, has just been created.");
                     break;
                 case "ranger":
                     // call new random ranger weapon assign
                     //call new Ranger(); function w given name from charName
+                    Console.WriteLine($"{charName}, your new Ranger, has just been created.");
                     break;
                 default:
                     Console.WriteLine("That is not a valid class for your character, try again.");
@@ -68,16 +64,48 @@ namespace TextBasedRPG
         }
     }
 
+    class Mage {
+        
+        public float mXP;
+        public float Mana;
+        public string mageName;
+        public string rngWeaponNameMage;
+        public string[] weapNamesMage = {"The Spark Stick", "Hand Warmer", "Spark Wand", "Amethyst Staff", "Kiekurakeppi", "Java Hell", "Boolin Boolean", "Eight", "Maximum Effort", "Cool Beans"};
+
+        public Mage(string name, int randomNumber) {
+
+            Mana = 10.0f;
+            mXP = 0f;
+            mageName = name;
+            
+            rngWeaponNameMage = weapNamesMage[randomNumber];
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string MenuTitle = "Main Menu";
+
+            titleUpdate(MenuTitle);
 
             charClass userCharacter = new charClass();
+            string cCreateTitle = "Character Creation";
+            titleUpdate(cCreateTitle);
             userCharacter.selectClass();
 
+            // open file and set variables for the users character stats here.
+            string gameBeginTitle = "Awakening";
+            titleUpdate(gameBeginTitle);
+        
             Console.ReadKey();
         }
+        static void titleUpdate(string newTitlePiece) {
+                
+                Console.Title = $"Placeholder - {newTitlePiece}";
+            }
     }
 }
