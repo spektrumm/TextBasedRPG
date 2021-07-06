70923801b942e2325ab8b477078d1467c6e4c501
nate hare (spekky) <51189697+spektrumm@users.noreply.github.com>
2021-07-06 15:51:45 -0700

    Save game implementation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,59 @@
 ﻿using System;
+using System.Text.Json;
+using System.IO;
 
 namespace TextBasedRPG
 {
-    class charClass {
+    class saveGame {
+
+        public string playerName { get; set; }
+        public float playerExp { get; set; }
+        public string currentScene { get; set; }
+        public string weaponName { get; set; }
+        public float resourceAmt { get; set; }
+
+        public static string mainPath = @"C:\Users\natha\Documents\TextBasedRPG";
+        public static string savesPath = @"C:\Users\natha\Documents\TextBasedRPG\saves";
+
+        
+        public saveGame(string name, float xp, float resource, string weapon, string scene) {
+
+            playerName = name;
+            playerExp = xp;
+            resourceAmt = resource;
+            weaponName = weapon;
+            currentScene = scene;
+        }
+        
+
+        public static void writeJson(string name, float xp, float resource, string weapon, string scene) {
+            
+            var saveFile = new saveGame(name, xp, resource, weapon, scene) {
+
+                playerName = name,
+                playerExp = xp,
+                resourceAmt = resource,
+                weaponName = weapon,
+                currentScene = scene
+
+            };
+
+            string fileName = $"{name}-{scene}-SaveGame.json";
+            string jsonString = JsonSerializer.Serialize(saveFile); // create the json data string
+            
+            File.WriteAllText(fileName, jsonString); // write the data to a file and name it accordingly
+            Console.WriteLine(File.ReadAllText(fileName)); // for debug purposes only
+
+            // move file to saves folder
+            string sourceFile = System.IO.Path.Combine(mainPath, fileName);
+            string destFile = System.IO.Path.Combine(savesPath, fileName);
+            System.IO.File.Move(sourceFile, destFile, true);
+            
+        }
+        
+    }
+    
+    public class charClass {
         public string classIn;
         public string userClass;
         //public string[] weapName = {""};
@@ -11,7 +62,7 @@ namespace TextBasedRPG
             // random number generator
             int numSelect = 0;
             Random ranNum = new Random();
-            numSelect = ranNum.Next(1,11);
+            numSelect = ranNum.Next(1,10);
 
             return numSelect;
 
@@ -36,9 +87,18 @@ namespace TextBasedRPG
                     
                     // print a line to the user describing the new Mage they have just created.
                     Console.WriteLine($"{playerMage.mageName}, your new Mage, has just been created.\nThey have {playerMage.Mana} mana points, and are using {playerMage.rngWeaponNameMage} as their weapon of choice.");
-                    
-                    //write to file to save this data for use in the rest of the game.
+                    string startingScene = "Awakening";
+                    saveGame initialSave = new saveGame(playerMage.mageName, playerMage.mXP, playerMage.Mana, playerMage.rngWeaponNameMage, startingScene);
 
+                    saveGame.writeJson(playerMage.mageName, playerMage.mXP, playerMage.Mana, playerMage.rngWeaponNameMage, startingScene);
+                    //write to file to save this data for use in the rest of the game.
+                    
+                    //newSave(playerMage.mageName, playerMage.mXP, playerMage.rngWeaponNameMage, startingScene);
+                    //saveGame file = new saveGame(playerMage.mageName, playerMage.mXP, playerMage.Mana, playerMage.rngWeaponNameMage, startingScene);
+                    
+                    //string jsonString = JsonSerializer.Serialize<saveGame>(saveGame);
+
+                    //Console.WriteLine(jsonString);
                     break;
                 case "rogue":
                     Rogue playerRogue = new Rogue(charName, randomNum); // call new mage constructor
@@ -75,7 +135,7 @@ namespace TextBasedRPG
         }
     }
 
-    class Mage {
+    public class Mage {
         
         public float mXP;
         public float Mana;
@@ -90,7 +150,8 @@ namespace TextBasedRPG
             "Programmer's Blessing", // special item (tooltip: "a tome of ancient knowledge with the words 'Lunnaya Vspyshka' ethched into the cover... Wonder what that could mean.")
             "Magic Eight Ball",
             "Rune of Kanai",
-            "Enchanted Icicle"};
+            "Enchanted Icicle",
+            "Basic Wand"};
 
         public Mage(string name, int randomNumber) {
 
@@ -103,7 +164,7 @@ namespace TextBasedRPG
         }
     }
 
-    class Rogue {
+    public class Rogue {
         
         public float roXP;
         public float Stealth;
@@ -132,7 +193,7 @@ namespace TextBasedRPG
         }
     }
 
-    class Ranger {
+    public class Ranger {
         
         public float raXP;
         public float Dexterity;
@@ -161,7 +222,7 @@ namespace TextBasedRPG
         }
     }
 
-    class Duelist {
+    public class Duelist {
         
         public float dXP;
         public float Strength;
